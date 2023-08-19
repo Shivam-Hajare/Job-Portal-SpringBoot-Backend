261dae331102fd797860dd8716f6594e45ac8b51
Shivam Hajare <shivamhajare96@gmail.com>
2023-08-19 19:39:41 +0530

    ready

--- src/main/java/com/app/Job_Portal/dto/SkillDto.java
+++ src/main/java/com/app/Job_Portal/dto/SkillDto.java
@@ -2,6 +2,7 @@ package com.app.Job_Portal.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
@@ -9,6 +10,7 @@ import lombok.ToString;
 @Setter
 @ToString
 @AllArgsConstructor
+@NoArgsConstructor
 public class SkillDto {
     private Long skillId;
     private String name;
