7a4ff70c754f35c10060646cb15d36a31e166918
Shivam Manohar Hajare <87773897+Shivam-Hajare@users.noreply.github.com>
2023-08-19 13:08:03 +0530

    Update JobSeekerRepository.java

--- src/main/java/com/app/Job_Portal/repository/JobSeekerRepository.java
+++ src/main/java/com/app/Job_Portal/repository/JobSeekerRepository.java
@@ -6,6 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface JobSeekerRepository extends JpaRepository<JobSeeker, Long> {
-
-    Optional<JobSeeker> findById(Long id);
 }
