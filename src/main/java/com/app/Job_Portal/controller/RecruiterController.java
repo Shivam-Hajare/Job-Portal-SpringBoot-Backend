e9ea30c85daeb1704b4308ccd499584f48af91ce
pranavchougule333 <pranavchougule333@gmail.com>
2023-08-23 17:02:54 +0530

    ok

--- src/main/java/com/app/Job_Portal/controller/RecruiterController.java
+++ src/main/java/com/app/Job_Portal/controller/RecruiterController.java
@@ -16,9 +16,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.app.Job_Portal.dto.JobApplicationsListDto;
-import com.app.Job_Portal.dto.JobSeekerRequestDto;
 import com.app.Job_Portal.dto.PostJobRequestDto;
 import com.app.Job_Portal.dto.RecruiterRequestDto;
 import com.app.Job_Portal.dto.UpdateJobRequestDto;
