011d8b5943351285063009a89a508d9a3948e013
Aditya Lavate <adityalavate2000@gmail.com>
2023-08-23 08:42:06 +0530

    deleted temporary files

--- src/main/java/com/app/Job_Portal/fileHandling/FileHandlingRepo.java
+++
@@ -1,6 +0,0 @@
-package com.app.Job_Portal.fileHandling;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FileHandlingRepo extends JpaRepository<ResumeTempo, Long> {
-}
